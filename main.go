4c503ec5d457f96224811f5b5a0caf37a5df5b91
PratikforCoding <kotalpratik@gmail.com>
2023-01-24 12:57:17 +0530

    Added flag -l to count lines
    Signed-off-by: PratikforCoding <kotalpratik@gmail.com>

--- main.go
+++ main.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
+	//defining the boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+	
+	//parsing the flag provided by the user
+	flag.Parse()
+
 	//Calling the count function to count the number of words
 	//received ferom the standard input and printing it out
-	fmt.Println(count(os.Stdin))
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// a scanner is used to read a text from a reader (such as files)
 	scanner := bufio.NewScanner(r)
 
 	// define the scanner split type to words (default is split by line)
-	scanner.Split(bufio.ScanWords)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// define a counter
 	wc := 0
--- main_test.go
+++ main_test.go
@@ -9,9 +9,20 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b)
+	res := count(b, false)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+// TestCountLines tests the count function set to count lines
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1") 
+	exp := 3
+	res := count(b, true)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead\n", exp, res)
 	}
 }
