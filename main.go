e8577d0242a531a76e075f9b183a11805a873294
PratikforCoding <kotalpratik@gmail.com>
2023-01-25 02:37:33 +0530

    Added -b flag for byte count
    Signed-off-by: PratikforCoding <kotalpratik@gmail.com>

--- main.go
+++ main.go
@@ -11,22 +11,28 @@ import (
 func main() {
 	//defining the boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
 	
 	//parsing the flag provided by the user
 	flag.Parse()
 
 	//Calling the count function to count the number of words
 	//received ferom the standard input and printing it out
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// a scanner is used to read a text from a reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// define the scanner split type to words (default is split by line)
+	// define the scanner split type to words or bytes(default is split by line)
 	if !countLines {
-		scanner.Split(bufio.ScanWords)
+		if !countBytes {
+			scanner.Split(bufio.ScanWords)
+		} else {
+			scanner.Split(bufio.ScanBytes)
+		}
+		
 	}
 
 	// define a counter
--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b, false)
+	res := count(b, false, false)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
@@ -20,7 +20,18 @@ func TestCountWords(t *testing.T) {
 func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1") 
 	exp := 3
-	res := count(b, true)
+	res := count(b, true, false)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead\n", exp, res)
+	}
+}
+
+// TestCountBytes tests the count funtion set to count bytes
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("bbbb")
+	exp := 4
+	res := count(b, false, true)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead\n", exp, res)
